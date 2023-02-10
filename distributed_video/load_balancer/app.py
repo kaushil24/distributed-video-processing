cf4a8172a081351d91ebae40a69994a04edc47df
phaneendra97 <phaneendra1997@gmail.com>
2023-02-09 16:54:37 -0800

    reading and saving file from http req

--- distributed_video/load_balancer/app.py
+++ distributed_video/load_balancer/app.py
@@ -2,13 +2,13 @@ from flask import Flask, request
 from http import HTTPStatus
 from flask import Response
 import json
-# from libs.utils import MediaUtils
-from decouple import config, Csv
 from pathlib import Path
 import time
 import uuid
 import os
 import zmq
+import re
+import base64
 
 app = Flask("load_balancer")
 
@@ -25,38 +25,26 @@ def test2():
 
     consumer_receiver = context.socket(zmq.PULL)
     consumer_receiver.bind("tcp://127.0.0.1:6000")
-    dummy = request.json
-    # uploded_file = request.files.get('file')
-    # request_dict = request.form
-    # file_name, ext = os.path.splitext(request_dict.get('file_name'))
-    # file_name = f'{file_name}__{time.strftime("%Y%m%d_%H%M%S")}.{ext}'
-    # # MediaUtils.save_video(uploded_file, Path(f'media/{file_name}'))
+
+    data = request.json
+    byteString = re.sub("^.*,", "", data['video'])
+    videoData = base64.b64decode(byteString)
+    with open('output.mp4', "wb") as out_file:  # open for [w]riting as [b]inary
+        out_file.write(videoData)
     # task_id = str(uuid.uuid4())
     response_dict = {'task_id': "asndkasnd3242qwsed"}
-    tasks = ["task1", "task2", "task3"]
-    for task in tasks:
-        work_message = { 'task' : task }
-        zmq_socket.send_json(work_message)
-    while len(tasks) is not 0:
-        print("done Sending")    
-        work = consumer_receiver.recv_json()
-        data = work['data']
-        print(data)
-        tasks.remove(data)
+    # tasks = ["task1", "task2", "task3"]
+    # for task in tasks:
+    #     work_message = { 'task' : task }
+    #     zmq_socket.send_json(work_message)
+    # while len(tasks) is not 0:
+    #     print("done Sending")    
+    #     work = consumer_receiver.recv_json()
+    #     data = work['data']
+    #     print(data)
+    #     tasks.remove(data)
 
     return Response(json.dumps(response_dict), HTTPStatus.OK, mimetype='application/json')
-
-@app.route("/get-triangles", methods=["POST"])
-def get_triangles():
-    """
-    video = request.form.video
-    videl_dir = video.save()
-
-    distribute_frame(video_dir)  # celery task
-
-    return Response({"tracking_id": sdsd})
-    """
-    pass
 
 if __name__ == '__main__':
     app.run(port=5000, debug=True)
